7d475c76ee504c035307edd077ed176c86863bf0
Stone-Zeng <pssysrq@163.com>
2017-01-14 01:58:07 +0100

    索引

---
+++ DELETE.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+DEL *.aux
+DEL *.log
+
+REM del *.bbl
+REM del *.blg
+
+DEL *.idx
+DEL *.ilg
+
+DEL *.out
+DEL *.thm
+
+REM DEL *.toc
+REM DEL *.listing
---
+++ MAKE.bat
@@ -0,0 +1,4 @@
+xelatex Notes
+zhmakeindex -s MyIndexStyle.mst -z pinyin Notes
+xelatex Notes
+xelatex Notes
